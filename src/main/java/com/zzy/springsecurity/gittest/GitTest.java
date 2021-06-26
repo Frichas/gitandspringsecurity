ad7a7d601b477fca18b8231f6409694a76f3ee72
Frichas <ziyuzeng1314@163.com>
2021-06-26 20:55:30 +0800

    test commit version2!

--- src/main/java/com/zzy/springsecurity/gittest/GitTest.java
+++ src/main/java/com/zzy/springsecurity/gittest/GitTest.java
@@ -8,6 +8,7 @@ package com.zzy.springsecurity.gittest;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("I am version1!!!");
+        //System.out.println("I am version1!!!");
+        System.out.println("I am version2!!!");
     }
 }
