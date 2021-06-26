e5b523652964bb18d133f2d8a1fac5ef74e7500d
Frichas <46220935+Frichas@users.noreply.github.com>
2021-06-26 21:07:40 +0800

    test commit version3 on github.
    test commit version3 on github.

--- src/main/java/com/zzy/springsecurity/gittest/GitTest.java
+++ src/main/java/com/zzy/springsecurity/gittest/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         //System.out.println("I am version1!!!");
         System.out.println("I am version2!!!");
+        System.out.println("I am version3!!!");
     }
 }
